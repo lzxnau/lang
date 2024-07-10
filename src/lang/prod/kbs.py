9cde5252b2b88aa341b6481377b5b98d2317ab3f
lzxnau <lzxnau@gmail.com>
2024-07-10 21:24:36 +1000

    update

--- src/lang/prod/kbs.py
+++ src/lang/prod/kbs.py
@@ -5,7 +5,13 @@ Version: 2024.07.10.01
 """
 
 import bs4
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
 from langchain_community.document_loaders import WebBaseLoader
+from langchain_core.vectorstores import VectorStoreRetriever
+from langchain_huggingface import HuggingFaceEmbeddings
+
+from src.lang.util.decorators import Timer
 
 
 class KB:
@@ -15,15 +21,34 @@ class KB:
         """Class initialization."""
         self.urls = urls
 
-    def get_retriever(self):
+        # Embedding model
+        self.em = "sentence-transformers/all-MiniLM-L6-v2"
+
+    @Timer.fxn_run
+    def get_retriever(self) -> VectorStoreRetriever:
         """Get retriever."""
         loader = WebBaseLoader(
             web_paths=self.urls,
             bs_kwargs=dict(
                 parse_only=bs4.SoupStrainer(
-                    class_=("post-content", "post-title", "post-header")
+                    class_=(
+                        "post-content",
+                        # "post-title",
+                        # "post-header"
+                    )
                 )
             ),
         )
         docs = loader.load()
-        print(docs)
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=250, chunk_overlap=20
+        )
+        doc_splits = text_splitter.split_documents(docs)
+        embeddings = HuggingFaceEmbeddings(model_name=self.em)
+        vectorstore = Chroma.from_documents(
+            documents=doc_splits,
+            embedding=embeddings,
+        )
+        retriever = vectorstore.as_retriever()
+
+        return retriever
---
+++ src/lang/util/decorators.py
@@ -0,0 +1,32 @@
+"""
+Lang Project Decorators Module in util.
+
+Version: 2024.07.10.01
+"""
+
+
+class Timer:
+    """Timer Decorator Class."""
+
+    @staticmethod
+    def fxn_run(fxn):
+        """Get running time of a function."""
+        import time
+
+        def wrapper(*args, **kwargs):
+            start_time = time.time()
+            result = fxn(*args, **kwargs)
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            minutes, seconds = divmod(int(elapsed_time), 60)
+            class_name = args[0].__class__.__name__ if args else fxn.__module__
+            if minutes == 0:
+                rtime = f"{seconds}s"
+            else:
+                rtime = f"{minutes}m {seconds}s"
+            text = f"{class_name}.{fxn.__name__} took {rtime} to execute."
+            print(text)
+
+            return result
+
+        return wrapper
