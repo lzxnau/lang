843c4f3ef8056ca0a9d7e97f6d1f1f3d560de5ec
lzxnau <lzxnau@gmail.com>
2024-07-12 12:21:38 +1000

    update

--- src/lang/prod/lc.py
+++ src/lang/prod/lc.py
@@ -4,10 +4,21 @@ Lang Chain Module.
 Version: 2024.07.11.01
 """
 
+from lang.util.decorators import Timer
+from langchain_core.runnables.base import Runnable
+
 
 class LC:
     """LC Class."""
 
-    def __init__(self):
+    def __init__(self, query: str, chain: Runnable):
         """Class initialization."""
-        pass
+        self.query: str = query
+        self.chain: Runnable = chain
+
+    @Timer.fxn_run
+    def get_output(self) -> None:
+        """Get chain output."""
+        for chunk in self.chain.stream(self.query):
+            print(chunk, end="", flush=True)
+        print()
--- src/lang/test/test.py
+++ src/lang/test/test.py
@@ -4,9 +4,8 @@ Test Model.
 Version: 2024.07.09.01
 """
 
-import time
-
 from lang.prod.kb import KB
+from lang.prod.lc import LC
 from lang.prod.lm import OLM
 from langchain import hub
 from langchain_core.output_parsers import StrOutputParser
@@ -19,7 +18,7 @@ class Test:
         """Class initialization."""
         self.name = "Test"
 
-    def test_cfg_rag(self) -> None:
+    def test_cfg_rag1(self) -> None:
         """Test config rag."""
         # Knowledgebase urls
         self.urls = [
@@ -29,14 +28,17 @@ class Test:
         # chat model name
         self.mn = "llama3"
 
-        self.test_prod()
+        # Query
+        self.query = "What is Task Decomposition?"
+
+        self.test_rag()
 
     def process(self) -> None:
         """Process function."""
         print(f"This is {self.name} class.\n")
-        self.test_cfg_rag()
+        self.test_cfg_rag1()
 
-    def test_prod(self) -> None:
+    def test_rag(self) -> None:
         """Test prod function."""
         kb = KB(self.urls)
         kb_chain = kb.get_chain()
@@ -47,13 +49,8 @@ class Test:
         llm = lm.get_model()
 
         rag_chain = kb_chain | prompt | llm | StrOutputParser()
-
-        stime = time.time()
-        for chunk in rag_chain.stream("What is Task Decomposition?"):
-            print(chunk, end="", flush=True)
-
-        etime = time.time()
-        print(f"\nrag_chain took {etime - stime} seconds.")
+        lc = LC(self.query, rag_chain)
+        lc.get_output()
 
     def test_lang(self) -> None:
         """Test Python Language function."""
