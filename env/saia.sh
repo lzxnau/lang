ddcc9e05c42026e56514b8425f44fe3b57b326cb
lzxnau <lzxnau@gmail.com>
2024-08-03 10:15:08 +1000

    update

--- env/saia.sh
+++ env/saia.sh
@@ -6,6 +6,6 @@ source env/lang/bin/activate || exit
 export CUDA_VISIBLE_DEVICES=""
 export PYTHONUNBUFFERED=1
 export TOKENIZERS_PARALLELISM="false"
-export USER_AGENT="LangChain Jeff"
+export USER_AGENT="Streamlit SAIA"
 export PYTHONPATH="$HOME"/pgit/lang/src
 python3.12 -m streamlit run src/lang/chat/chatbot.py
